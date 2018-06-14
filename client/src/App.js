4c555dec0374ba0c732ac0f1aaf252dcc3dace40
Juan Fox <foxj0875@gmail.com>
2018-06-14 12:14:15 -0400

    commented out front end socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import socketIOClient from "socket.io-client";
+//import socketIOClient from "socket.io-client";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    const socket = socketIOClient('http://localhost:3001');
+    //const socket = socketIOClient('http://localhost:3001');
     return (
       <div className="App">
         <header className="App-header">
