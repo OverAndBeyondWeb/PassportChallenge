06c356c9059933c73bdaf000b2a0f60a2359a42e
Juan Fox <foxj0875@gmail.com>
2018-06-17 19:00:26 -0400

    comments

--- models/Factory.js
+++ models/Factory.js
@@ -1,8 +1,10 @@
-//reference mongoose library
+// Reference mongoose library
 const mongoose = require('mongoose');
 
+// Schema Object
 const Schema = mongoose.Schema;
 
+// Define factory schema
 const FactorySchema = new Schema({
   name: {
     type: String
@@ -13,6 +15,8 @@ const FactorySchema = new Schema({
   }]
 });
 
+// Create model from schema
 const Factory = mongoose.model('Factory', FactorySchema);
 
+// Export model
 module.exports = Factory;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,43 +1,83 @@
-// api routes
+// Api routes
+
+// Dependencies
 const router = require('express').Router();
 const Factory = require('../models/Factory');
 const randomNumber = require('../utils/randomNumber');
-const sse = require('sse-express');
-const app = require('express')();
+
 
 module.exports = app => {
 
+  // Create a factory
   router.post('/api/factory', (req, res) => {
+
+    // From name form field
     let name = req.body.name;
+
+    // Pass numChildren, upperbound, and lowerbound to the createChildren
+    // function to create specified amount of random numbers,
+    // between specified 2 bounds
     let children = createChildren(req.body.numChildren, req.body.lowerbound, req.body.upperbound);
-    console.log(children, name)
+
+    // Insert name and random numbers into the database
     Factory.create({name: name, children: children})
+
+      // Emit an event after the data has been entered
       .then(res => app.emit('message', {title: 'New message!'}))
+
+      // Show errors on the console
       .catch(err  => console.log(err));    
   });
   
+  // Delete 1 factory
   router.delete('/api/factory/:id', (req, res) => {
+
+    // Remove factory with _id that matches id from the url
     Factory.deleteOne({_id: req.params.id})
+
+      // Emit an event after the data has been entered
       .then(res => app.emit('message', {title: 'New message!'}))
+
+      // Show errors on the console
       .catch(err  => console.log(err))
   });
-  
+
+  // Delete all factories
   router.delete('/api/factories', (req, res) => {
+
+    // Remove every entry from the factories collection
     Factory.remove({})
+
+      // Emit an event after the data has been entered
       .then(res => app.emit('message', {title: 'New message!'}))
+
+      // Show errors on the console
       .catch(err  => console.log(err))
   });
-  
+
+  // Rename a factory
   router.put('/api/factory/:id', (req, res) => {
+
+    // Replace name in the database, of the factory with _id that matches
+    // id from the url, with name from the front end field
     Factory.findByIdAndUpdate(req.params.id, { name: req.body.newName })
+
+    // Emit an event after the data has been entered
     .then(res => app.emit('message', {title: 'New message!'}))
+
+    // Show errors on the console
     .catch(err => console.log(err))
   });
 
+  // Retrieve all factories
   router.get('/api/factories', (req, res) => { 
-    console.log();
+
     Factory.find({})
+
+      // Display factories as json
       .then(factory => res.status(200).json(factory))
+
+      // Show errors on the console
       .catch(err => console.log(err));
   });
 
